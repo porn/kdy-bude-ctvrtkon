7ef3cd5a5153c17579463e9b6061a8efdd3bb298
Ikar Pohorsky <pohorsky@inizio.cz>
2023-09-11 13:28:51 +0200

    update source feed parsing (fix #1)

--- kdy-bude-ctvrtkon.py
+++ kdy-bude-ctvrtkon.py
@@ -4,13 +4,13 @@ import urllib.request
 from datetime import datetime, timedelta
 from typing import List
 
-CTVRTKON_FEED = 'https://www.ctvrtkon.cz/feed-1.json'
+CTVRTKON_FEED = 'https://ctvrtkon.cz/api/events/feed'
 
 
 def load_items() -> List[dict]:
     try:
         with urllib.request.urlopen(CTVRTKON_FEED) as f:
-            return json.loads(f.read().decode('utf-8'))['items']
+            return json.loads(f.read().decode('utf-8'))['data']
     except Exception:
         print(f"Nepodařilo se mi fetchnout feed;( Co tobě?\n{CTVRTKON_FEED}")
         exit(1)
@@ -18,10 +18,10 @@ def load_items() -> List[dict]:
 
 def filter_items_older_than(items: List[dict], allowed_date: datetime) -> List[dict]:
     for i in items:
-        if i.get('time') is None:
+        if i.get('started_at') is None:
             continue
 
-        event_time = datetime.strptime(i['time'], '%d.%m.%Y %H:%M')
+        event_time = datetime.strptime(i['started_at'], '%Y-%m-%d %H:%M:%S')
         if event_time >= allowed_date:
             i['datetime'] = event_time
             yield i
@@ -29,16 +29,16 @@ def filter_items_older_than(items: List[dict], allowed_date: datetime) -> List[d
 
 def print_item(item):
     try:
-        print('╔═' + '═' * len(item['title']) + '═╗')
-        print(f"║ {item['title']} ║")
-        print('╚═' + '═' * len(item['title']) + '═╝')
+        print('╔═' + '═' * len(item['name']) + '═╗')
+        print(f"║ {item['name']} ║")
+        print('╚═' + '═' * len(item['name']) + '═╝')
         print()
 
         print(f"{item['description']}\n")
-        print(f"Kdy:  {item['time']}")
-        print(f"Kde:  {item['place']}")
-        print(f"Link: {item['url']}")
-        print(f"FB:   {item['fbEventLink']}")
+        print(f"Kdy:        {item['started_at']}")
+        print(f"Kde:        {item['venue']['name']}; {item['venue']['address']} ({item['venue']['website_url']})")
+        print(f"Registrace: {item['registration_url']}")
+        print(f"FB:         {item['facebook_url']}")
         print()
 
     except KeyError:
@@ -48,7 +48,7 @@ def print_item(item):
 if __name__ == '__main__':
     today = datetime.today()
     # today = datetime.today().replace(day=29, hour=19)
-    # today = datetime.today().replace(year=2018, month=1)
+    # today = datetime.today().replace(year=2023, month=8)
     # print(today)
 
     items = load_items()
